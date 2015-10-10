4db3462575360ca698f8bd9810e3f78bbfd052f9
Winterstorm <Winterstorm@M400>
2015-10-10 10:07:17 -0700

    Line fixed

--- asciiRPG/src/world/Line.java
+++ asciiRPG/src/world/Line.java
@@ -41,13 +41,12 @@ public class Line implements Iterable<Point> {
     public Line(int x0, int y0, int x1, int y1) {
         this.points = new ArrayList<Point>();
 
-        int dx = x1 - x0;
-        int dy = y1 - y0;
+        int dx = Math.abs(x1 - x0);
+        int dy = Math.abs(y1 - y0);
 
         int sx = x0 < x1 ? 1 : -1;
         int sy = y0 < y1 ? 1 : -1;
         int err = dx - dy;
-        int e2;
 
         while (true) {
             this.points.add(new Point(x0, y0));
@@ -55,7 +54,7 @@ public class Line implements Iterable<Point> {
                 break;
             }
 
-            e2 = err * 2;
+            int e2 = err * 2;
             if (e2 > -dy) {
                 err -= dy;
                 x0 += sx;
